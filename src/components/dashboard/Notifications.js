40569a97fb3e53940d91a97edd6d0fc4b20fca41
ankitpal1029 <ankitkumarpal27@gmail.com>
2020-10-15 02:02:25 +0530

    deployed to netlify have to make own backend

--- src/components/dashboard/Notifications.js
+++ src/components/dashboard/Notifications.js
@@ -3,7 +3,17 @@ import React from 'react'
 const Notifications = () => {
     return (
         <div>
-            <p>Notifications</p>
+            <div className="card z-depth-0">
+                <div className="card-content">
+                    <div className="card-title">Notifications</div>
+                    <ul>
+                        <li>Notifications</li>
+                        <li>Notifications</li>
+                        <li>Notifications</li>
+                        <li>Notifications</li>
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/projects/MemeDetails.js
+++ src/components/projects/MemeDetails.js
@@ -3,7 +3,6 @@ import { connect } from 'react-redux'
 import { firestoreConnect } from 'react-redux-firebase'
 import { compose } from 'redux'
 import { Redirect } from 'react-router-dom'
-import DayJS from 'react-dayjs'
 import moment from 'moment'
 
 
